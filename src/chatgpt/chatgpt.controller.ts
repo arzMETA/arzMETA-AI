327507bd1907918b93c9556fa21ad19bf02fe616
jaemyeongson <jaemyeongson@kakao.com>
2023-06-26 17:48:11 +0900

    손재명 / 연구원 - 테스트 코드 추가

--- src/chatgpt/chatgpt.controller.ts
+++ src/chatgpt/chatgpt.controller.ts
@@ -28,25 +28,25 @@ export class ChatgptController {
   //     return await this.chatgptService.chatGPT(data);
   //   }
 
-  @ApiOperation({ summary: '헬스트레이너' })
-  //   @UseGuards(LoggedInGuard)
-  @Post('trainer')
-  async getChatGPTFrontis(@Body() data: CreateChatGPTDTO) {
-    return await this.chatgptService.trainer(data);
-  }
+  // @ApiOperation({ summary: '헬스트레이너' })
+  // //   @UseGuards(LoggedInGuard)
+  // @Post('trainer')
+  // async getChatGPTFrontis(@Body() data: CreateChatGPTDTO) {
+  //   return await this.chatgptService.trainer(data);
+  // }
 
-  @ApiOperation({ summary: '영양사' })
-  //   @UseGuards(LoggedInGuard)
-  @Post('nutritionist')
-  async nutritionist(@Body() data: CreateChatGPTDTO) {
-    return await this.chatgptService.nutritionist(data);
-  }
-  @ApiOperation({ summary: '한국어 선생님' })
-  //   @UseGuards(LoggedInGuard)
-  @Post('koreanTeacher')
-  async koreanTeacher(@Body() data: CreateChatGPTDTO) {
-    return await this.chatgptService.koreanTeacher(data);
-  }
+  // @ApiOperation({ summary: '영양사' })
+  // //   @UseGuards(LoggedInGuard)
+  // @Post('nutritionist')
+  // async nutritionist(@Body() data: CreateChatGPTDTO) {
+  //   return await this.chatgptService.nutritionist(data);
+  // }
+  // @ApiOperation({ summary: '한국어 선생님' })
+  // //   @UseGuards(LoggedInGuard)
+  // @Post('koreanTeacher')
+  // async koreanTeacher(@Body() data: CreateChatGPTDTO) {
+  //   return await this.chatgptService.koreanTeacher(data);
+  // }
 
   
   @ApiOperation({ summary: '노무사 AI' })
